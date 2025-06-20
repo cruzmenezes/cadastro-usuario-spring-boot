67e0bc7d282c7be314032fcce46973e4c41abaee
cruzmenezes <cruz.menezes@outlook.com>
2025-06-20 08:46:21 -0300

    update springboot

---
+++ cadastro-usuario/src/test/java/com/projetojava/cadastro_usuario/CadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojava.cadastro_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
